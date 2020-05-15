c1775cea54088c1e75f2ecc1c2fdaf59f95490fd
Rachel <rachelgoree26@gmail.com>
2020-05-15 14:56:04 +0200

    added NumberOfEvents

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
+    state = {
+        query: 32, //default event number per page for meetup
+    }
+
+    handleInputChanged = (event) => {
+        const value = event.target.value;
+        this.setState({ query: value });
+    }
+
     render() {
         return (
             <div className="NumberOfEvents">
-                <input type="number" className="number"></input>
+                <input type="number" className="number" value={this.state.query} onChange={this.handleInputChanged}></input>
             </div>
         );
     }
--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -3,8 +3,23 @@ import { shallow } from 'enzyme';
 import NumberOfEvents from '../NumberOfEvents';
 
 describe('<NumberOfEvents /> component', () => {
+    let NumberOfEventsWrapper;
+    beforeAll(() => {
+        NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+    });
+
     test('render text input', () => {
-        const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
         expect(NumberOfEventsWrapper.find('.number')).toHaveLength(1);
+    });
+
+    test('render number input correctly', () => {
+        const query = NumberOfEventsWrapper.state('query');
+        expect(NumberOfEventsWrapper.find('.number').prop('value')).toBe(query);
+    });
+
+    test('change state when number input changes', () => {
+        const eventObject = { target: { value: 5 } };
+        NumberOfEventsWrapper.find('.number').simulate('change', eventObject);
+        expect(NumberOfEventsWrapper.state('query')).toBe(5);
     });
 });
