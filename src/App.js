a0df40d5b64e1effcafda3ee148b834d3c895d5b
Rachel <rachelgoree26@gmail.com>
2020-05-20 14:55:05 +0200

    4.4 integration testing & adding accesstoken to get api data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
-import Event from './Event';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import { mockEvents } from './mock-events';
+import { getEvents } from './api';
+
 class App extends Component {
+  state = {
+    events: []
+  }
+
+  updateEvents = (lat, lon) => {
+    getEvents(lat, lon).then(events => this.setState({ events }));
+  }
   render() {
     return (
       <div className="App">
-        <CitySearch />
-        <EventList />
-        <Event />
+        <CitySearch updateEvents={this.updateEvents} />
         <NumberOfEvents />
+        <EventList events={this.state.events} />
+
+
       </div>
     );
   }
--- src/Event.js
+++ src/Event.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { mockEvents } from './mock-events';
 
 class Event extends Component {
     state = {
@@ -17,12 +18,20 @@ class Event extends Component {
             <div className="eventsList">
                 <div className="eventDetails">
 
-                    <div key={event.id}><p>{event.name}</p>
-                        <button className="showDetailsButton" onClick={this.handleShowDetails}>Show Details</button>
-                        {this.state.showDetails && (
-                            <p class="eventDescription">
+                    <div key={event.id}>
+                        <div className='eventDate'>Date: {event.local_date}</div>
+                        <div className='eventTime'>Time: {event.local_time}</div>
+                        <p className="name">{event.name}</p>
+                        <div className="group-name">Group: {event.group}</div>
+                        <div className="going">{event.yes_rsvp_count} are going </div>
+
+
+                        <button className="showDetailsButton details-btn" onClick={this.handleShowDetails}>Show Details</button>
+                        {this.state.showDetails &&
+                            <div className="description">
                                 {event.description}
-                            </p>)}
+                            </div>
+                        }
                     </div>
 
                 </div>
